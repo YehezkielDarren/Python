162c47fd7902ebd0cf5162b8ab16dc9a0a01036f
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-05-27 21:48:25 +0700

    Update rekursifPenjumlahan.py

--- PrAlPro/Pertemuan_14/rekursifPenjumlahan.py
+++ PrAlPro/Pertemuan_14/rekursifPenjumlahan.py
@@ -3,5 +3,12 @@ def perkalian(bil1,bil2):
         return bil1
     else:
         return bil1+perkalian(bil1,bil2-1)
+    
+def perkalianfor(bil1,bil2):
+    hasil=0
+    for i in range(bil2):
+        hasil+=bil1
+    return hasil
 
 print(perkalian(90,99))
+print(perkalianfor(90,99))
