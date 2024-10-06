4d73ed55a9805bb53e30773b461222dee465145c
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-10-06 22:11:14 +0700

    Create buatJson.py

---
+++ PrStrukDat/buatJson.py
@@ -0,0 +1,30 @@
+import json
+#menulis data di json
+# darren=dict()
+# darren["nim"]="71231023"
+# darren['nama'] = 'Yehezkiel Darren'
+# darren['prodi'] = 'Informatika'
+# darren['IPK'] = 3.87
+
+# bambang = dict()
+# bambang['nim'] = "9288818881"
+# bambang['nama'] = 'Bambang Gentolet'
+# bambang['prodi'] = 'Sistem Informasi'
+# bambang['IPK'] = 5.00
+
+# data_mhs=[darren,bambang]
+
+# with open("datamhs.json","w") as fjson:
+#     json.dump(data_mhs,fjson)
+
+# membaca data di json
+# with open("datamhs.json","r") as fjson:
+#     data=json.load(fjson)
+#     print(data)
+#     print("Daftar Mahasiswa")
+#     for mhs in data:
+#         print(mhs["nim"],"-",mhs["nama"])
+#         print("Prodi : ",mhs["prodi"] )
+#         print("IPK : ",mhs["IPK"])
+#         print()
+#     fjson.close()
