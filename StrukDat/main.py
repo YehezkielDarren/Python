14a9c27cc153af3bd4f673444e5f600a88749d27
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-10-08 11:45:10 +0700

    Update main.py

--- StrukDat/main.py
+++ StrukDat/main.py
@@ -1,19 +1,54 @@
-from Single_Linked_List import NonCircular
-import time
+# from Single_Linked_List import NonCircular
+# import time
+from Stack import *
 
 if __name__ == '__main__':
-    linkedList = NonCircular()
-    _i=int(input("Masukkan jumlah data : "))
-    for i in range(_i):
-        data=input("Masukkan Data : ")
-        linkedList.insert_head(data)
-    linkedList.print()
-    data_recent=input("Masukkan Data Lama : ")
-    data_update=input("Masukkan Data Baru : ")
-    # linkedList.delete_head() -- sudah berhasil
-    linkedList.update(data_recent,data_update) # sudah berhasil
-    linkedList.print()
-    # linkedList.delete_tail() -- sudah berhasil
-    time.sleep(10)
-    linkedList.delete_mid(1)
-    linkedList.print()
+    # LINKED LIST NON CIRCULAR BROOO
+    # linkedList = NonCircular()
+    # _i=int(input("Masukkan jumlah data : "))
+    # for i in range(_i):
+    #     data=input("Masukkan Data : ")
+    #     linkedList.insert_head(data)
+    # linkedList.print()
+    # data_recent=input("Masukkan Data Lama : ")
+    # data_update=input("Masukkan Data Baru : ")
+    # # linkedList.delete_head() -- sudah berhasil
+    # linkedList.update(data_recent,data_update) # sudah berhasil
+    # linkedList.print()
+    # # linkedList.delete_tail() -- sudah berhasil
+    # time.sleep(10)
+    # linkedList.delete_mid(1)
+    # linkedList.print()
+
+    # STACK LIST BROOOO
+    # stack_list=Stack_List()
+    # stack_list.push(1)
+    # stack_list.push(2)
+    # stack_list.push(3)
+    # stack_list.push(4)
+    # stack_list.push(5)
+    # print("Panjang stack : ",stack_list.len())
+    # print("Top : ",stack_list.top())
+    # print("Stack berhasil di pop : ",stack_list.pop_stack())
+    # print("Panjang stack : ",stack_list.len())
+    # print("Top : ",stack_list.top())
+    # print(stack_list.stack_data)
+
+    # STACK SLLNC BROOO
+    # stack_sllnc=Stack_SLLNC()
+    # stack_sllnc.push(1)
+    # stack_sllnc.push(2)
+    # stack_sllnc.push(3)
+    # stack_sllnc.push(4)
+    # stack_sllnc._stack_data.print()
+    # print()
+    # print("Panjang stack : ",stack_sllnc._stack_data._size)
+    # print("Stack berhasil di pop : ",stack_sllnc.pop())
+    # print("Panjang stack : ",stack_sllnc._stack_data._size)
+    # print()
+    # stack_sllnc._stack_data.print()
+    # print()
+    # print(stack_sllnc.clear())
+    # print()
+    # stack_sllnc.stack_data.print()
+    pass
