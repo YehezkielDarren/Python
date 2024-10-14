62db1ce65274e391e881227609282a1d2080feea
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2024-10-14 07:43:28 +0700

    Create Intopos.py

---
+++ StrukDat/Intopos.py
@@ -0,0 +1,12 @@
+class inToPos:
+    def __init__(self):
+if __name__=="__main__":
+    print("=== Mengubah Infix menjadi Postfix ===")
+    print("Ketentuan Operasi: ")
+    print("1. perkalian : (*)")
+    print("2. pembagian : (/)")
+    print("3. penjumlahan : (+)")
+    print("4. pengurangan : (-)")
+    infix=input("Masukkan soal matematika : ")
+
+    pass
